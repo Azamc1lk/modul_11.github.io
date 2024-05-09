df98f9dfcf4454fd112c1dcd1b15c295719e2dac
Azamc1lk <azamat khxolmirzayev@example.com>
2024-05-09 15:02:29 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+
+tg.MainButton.textColor="#FFFFFF"
+tg.MainButton.color="#FC3005"
+
+let btn1 = document.getElementById("btn1")
+btn1.addEventListener("click",function (){
+    tg.MainButton.setText("btn1 bosildi");
+    tg.MainButton.show();
+})
