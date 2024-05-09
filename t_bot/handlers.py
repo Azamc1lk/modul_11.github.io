572d95d098768d030ed9b9f71a05d110789cb219
Azamc1lk <azamat khxolmirzayev@example.com>
2024-05-09 15:27:25 +0500

    start

--- t_bot/handlers.py
+++ t_bot/handlers.py
@@ -1,10 +1,15 @@
-from aiogram import Dispatcher,types
+from aiogram import Dispatcher, types, F
 from aiogram.filters import CommandStart
 
 from t_bot.keyboards import app_ikb
 
-dp= Dispatcher()
+dp = Dispatcher()
+
 
 @dp.message(CommandStart())
-async def start(msg:types.Message):
-    await msg.answer("Salom",reply_markup=app_ikb)
+async def start(msg: types.Message):
+    await msg.answer("Salom", reply_markup=app_ikb)
+
+@dp.message(F.func(lambda msg: msg.web_app_data.data == "TestMessage"))
+async def get_btn(msg: types.Message):
+    await msg.answer(msg.web_app_data)
