77b52ae6d02993f23929369b67f80282dc4325d3
Azamc1lk <azamat khxolmirzayev@example.com>
2024-05-09 16:31:59 +0500

    start

--- app.js
+++ app.js
@@ -9,17 +9,16 @@ let btn2 = document.getElementById("btn2");
 let item = ""
 btn1.addEventListener("click", function () {
     tg.MainButton.setText("btn1 bosildi");
-    item= " button 1 bosildi"
+    item = " button 1 bosildi"
     tg.MainButton.show();
 });
 
 btn2.addEventListener("click", function () {
     tg.MainButton.setText("btn2 bosildi");
-    item= " button 2 bosildi"
+    item = " button 2 bosildi"
     tg.MainButton.show();
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
     tg.sendData(item)
 });
-
