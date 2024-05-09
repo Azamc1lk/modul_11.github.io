148a632627def3e4730bf4134b893c1e61a1a6ca
Azamc1lk <azamat khxolmirzayev@example.com>
2024-05-09 15:38:52 +0500

    start

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ tg.MainButton.textColor = "#FFFFFF"
 tg.MainButton.color = "#FC3005"
 
 let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let item = ""
 btn1.addEventListener("click", function () {
     tg.MainButton.setText("btn1 bosildi");
+    item= " button 1 bosildi"
     tg.MainButton.show();
 });
+
+btn2.addEventListener("click", function () {
+    tg.MainButton.setText("btn2 bosildi");
+    item= " button 2 bosildi"
+    tg.MainButton.show();
+});
+
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    tg.sendData("TestMessage")
+    tg.sendData(item)
 });
 
