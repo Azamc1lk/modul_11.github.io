5c441f41325fe9192d5264da9e9be7312d35f769
Azamc1lk <azamat khxolmirzayev@example.com>
2024-05-11 16:10:46 +0500

    start

--- app.js
+++ app.js
@@ -4,36 +4,52 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF"
 tg.MainButton.color = "#FC3005"
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let item = ""
-btn1.addEventListener("click", function () {
-    tg.MainButton.setText("kola bosildi");
-    item = " kola tanlandi  narx:3$"
+let rbtn1 = document.getElementById("rbtn1");
+let abtn1 = document.getElementById("abtn1");
 
+let rbtn2 = document.getElementById("rbtn2");
+let abtn2 = document.getElementById("abtn2");
+
+let rbtn3 = document.getElementById("rbtn3");
+let abtn3 = document.getElementById("abtn3");
+
+let rbtn4 = document.getElementById("rbtn4");
+let abtn4 = document.getElementById("abtn4");
+
+let item = " "
+let num_count = 0
+
+number = document.getElementById("count");
+
+abtn1.addEventListener("click", function () {
+
+    number.innerText = num_count += 1;
+    number.style.display = "block";
+})
+
+
+btn1.addEventListener("click", function () {
+    tg.MainButton.setText("Burger " + num);
+    item = "Burger/3$";
     tg.MainButton.show();
 });
-
 btn2.addEventListener("click", function () {
-    tg.MainButton.setText("lavash bosildi");
-    item = " lavash tanlandi narx:9$ "
-
+    tg.MainButton.setText("KFC");
+    item = "KFC/10$";
     tg.MainButton.show();
 });
 btn3.addEventListener("click", function () {
-    tg.MainButton.setText("burger bosildi");
-    item = " burger tanlandi  narx:8$"
-
-
+    tg.MainButton.setText("LAVASH");
+    item = "LAVASH/4$";
     tg.MainButton.show();
 });
 btn4.addEventListener("click", function () {
-    tg.MainButton.setText("pizza bosildi");
-    item = " pizza tanlandi  narxi:4$"
+    tg.MainButton.setText("Pizza");
+    item = "Pizza/5$";
 
     tg.MainButton.show();
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    tg.sendData(item)
+    tg.sendData(item);
 });
