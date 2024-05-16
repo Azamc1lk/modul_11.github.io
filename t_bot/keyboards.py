07ba30bd3eb9b18c6fb48d6623f810c44166aa0c
Azamc1lk <azamat khxolmirzayev@example.com>
2024-05-16 15:17:22 +0500

    start

--- t_bot/keyboards.py
+++ t_bot/keyboards.py
@@ -1,8 +1,11 @@
-from aiogram import Bot
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
 
-web_app = WebAppInfo(url="https://azamc1lk.github.io/modul_11.github.io/")
+web_app = WebAppInfo(url='https://bunyodnaimov.github.io/modul_11.github.io/')
 
 app_kb = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Mini app', web_app=web_app)]
+    [KeyboardButton(text='Mini App', web_app=web_app)]
 ], resize_keyboard=True)
+
+buy_ikb = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="To'lov", callback_data='buy')],
+])
