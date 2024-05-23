0f71478507a80c2e29210fdeb3e3426ec593c0ac
Azamc1lk <azamat khxolmirzayev@example.com>
2024-05-23 15:45:12 +0500

    start

--- t_bot/keyboards.py
+++ t_bot/keyboards.py
@@ -1,6 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
 
-web_app = WebAppInfo(url='https://bunyodnaimov.github.io/modul_11.github.io/')
+web_app = WebAppInfo(url='https://azamc1lk.github.io/modul_11.github.io/')
 
 app_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Mahsulotlarni korish', web_app=web_app)]
